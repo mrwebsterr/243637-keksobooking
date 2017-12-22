cb65f910f9c4496b325196454f0d18ef8b094760
Anar Amirov <amirov.anar@icloud.com>
2017-12-23 00:41:30 +0400

    Исправил замечания.

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -3,12 +3,8 @@
 (function () {
   window.synchronize = function (el1, el2, mappingElement, callback) {
     el1.addEventListener('change', function (evt) {
-      var el2Value;
-      if (el1.tagName === 'SELECT') {
-        el2Value = mappingElement[evt.target.options[evt.target.options.selectedIndex].value];
-      } else {
-        el2Value = mappingElement[evt.target.value];
-      }
+      var selectedOptionValue = evt.target.options[evt.target.options.selectedIndex].value;
+      var el2Value = el1.tagName === 'SELECT' ? mappingElement[selectedOptionValue] : mappingElement[evt.target.value];
       callback(el2, el2Value);
     });
   };
