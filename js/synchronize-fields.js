6621c76aeb78fa8155765f12ca97b6bf5e358a38
Anar Amirov <amirov.anar@icloud.com>
2017-12-22 16:03:07 +0400

    Переделал функцию синхронизации полей.

--- js/synchronize-fields.js
+++ js/synchronize-fields.js
@@ -1,23 +1,16 @@
 'use strict';
 
 (function () {
-  window.synchronize = function (el1, el2, mappingElement) {
-    if (!mappingElement) {
-      el1.addEventListener('change', function (evt) {
-        for (var i = 0; i < el2.length; i++) {
-          el2[i].removeAttribute('selected');
-          if (evt.target.value === el2[i].value) {
-            el2[i].setAttribute('selected', 'selected');
-          }
-          if (evt.target.value === '100') {
-            el2[3].setAttribute('selected', 'selected');
-          }
-        }
-      });
-    } else {
-      el1.addEventListener('change', function (mapEvt) {
-        return (el2.minLength = mappingElement[mapEvt.target.value]);
-      });
-    }
+  window.synchronize = function (el1, el2, mappingElement, callback) {
+    el1.addEventListener('change', function (evt) {
+      var el2Value;
+      if (el1.tagName === 'SELECT') {
+        el2Value = mappingElement[evt.target.options[evt.target.options.selectedIndex].value];
+      } else {
+        el2Value = mappingElement[evt.target.value];
+      }
+      callback(el2, el2Value);
+    });
   };
 })();
+
