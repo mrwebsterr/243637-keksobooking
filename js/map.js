0bb597e43b84795ad4af3602dae4db0f5326be6c
Anar Amirov <amirov.anar@icloud.com>
2017-12-22 16:05:37 +0400

    Добавил дополнительные объекты-словари.

--- js/map.js
+++ js/map.js
@@ -4,18 +4,52 @@
   var mapBlock = document.querySelector('.map');
   var mapFiltersContainer = document.querySelector('.map__filters-container');
   var featuresListTemplate = document.querySelector('template').content.querySelector('.popup__features');
+  var notice = document.querySelector('.notice');
+  var roomCount = notice.querySelector('#room_number');
+  var capacity = notice.querySelector('#capacity');
   var lodgingType = document.querySelector('#type');
   var lodgingPrice = document.querySelector('#price');
+  var timeInField = notice.querySelector('#timein');
+  var timeOutValue = notice.querySelector('#timeout');
   var lodgingTypeToMinPrice = {
     'bungalo': '0',
     'flat': '1000',
     'house': '5000',
     'palace': '10000'
   };
-  var roomNumber = document.querySelector('#room_number');
-  var guestsNumberValue = document.querySelectorAll('#capacity option');
-  var timeInField = document.querySelector('#timein');
-  var timeOutValue = document.querySelectorAll('#timeout option');
+  var roomToCapacity = {
+    1: ['1'],
+    2: ['1', '2'],
+    3: ['1', '2', '3'],
+    100: ['noGuests']
+  };
+  var timeInToTimeOut = {
+    '12:00': '12:00',
+    '13:00': '13:00',
+    '14:00': '14:00'
+  };
+  var capacityMap = {
+    1: {
+      value: '1',
+      text: 'для 1 гостя'
+    },
+    2: {
+      value: '2',
+      text: 'для 2 гостей'
+    },
+    3: {
+      value: '3',
+      text: 'для 3 гостей'
+    },
+    noGuests: {
+      value: '0',
+      text: 'не для гостей'
+    }
+  };
+  var setMinValue = function (element, type) {
+    element.minLength = type;
+  };
+
   var similarAds = [];
   var generateSimilarAds = function (adsCount) {
     for (var i = 0; i < adsCount; i++) {
@@ -45,6 +79,17 @@
       });
     }
   };
+  var updateSelectOptions = function (select, options) {
+    window.util.removeChildren(select);
+    options.forEach(function (item) {
+      var option = document.createElement('option');
+      option.value = capacityMap[item].value;
+      option.textContent = capacityMap[item].text;
+      select.appendChild(option);
+    });
+  };
+
+
   var onMainPinMouseup = function () {
     mapBlock.classList.remove('map--faded');
     window.util.removeChildren(featuresListTemplate);
@@ -54,9 +99,10 @@
     window.util.closePopup();
     window.form.enableFormFields();
     window.form.checkValidity();
-    window.synchronize(roomNumber, guestsNumberValue);
-    window.synchronize(timeInField, timeOutValue);
-    window.synchronize(lodgingType, lodgingPrice, lodgingTypeToMinPrice);
+    window.synchronize(roomCount, capacity, roomToCapacity, updateSelectOptions);
+    window.synchronize(timeInField, timeOutValue, timeInToTimeOut, window.util.selectByValue);
+    window.synchronize(timeOutValue, timeInField, timeInToTimeOut, window.util.selectByValue);
+    window.synchronize(lodgingType, lodgingPrice, lodgingTypeToMinPrice, setMinValue);
     window.pin.mainPin.removeEventListener('mouseup', onMainPinMouseup);
   };
   window.form.disableFormFields();
